dee196190591a3512ffa18a28bded0a9230a6b0d
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-04-28 10:36:06 +0700

    Add files via upload

---
+++ hack-voucher.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+#tools kode voucher
+#by acep
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS Hack Voucher          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : Acep X-Code       |"
+echo  $red    "| Contact       : 085797616471      |"
+echo  $red    "| My Team       : Indo Team         |"
+echo  $red    "| Thanks To     : Holilul Anwar     |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Indosat                 $blue ||"
+echo $blue "|| $green 0. Exit The Program        $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@acepx-code=•>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+toilet -f standard "Acep Gans" -F gay
+echo $blue"Ketik *556*Kode voucher#"
+sleep 1
+echo $green"Sedang Mengumpulkan Kode"
+sleep 1
+echo $red "Loading......"
+sleep 3
+echo "===================="
+echo $purple "48120404907561"
+sleep 3
+echo "===================="
+echo $green "19075621249751"
+sleep 4
+echo "===================="
+echo $blue "21234587498234"
+sleep 5
+echo "===================="
+echo $cyan "12389237019365"
+sleep 6
+echo "===================="
+echo $red "83626483638294"
+echo "===================="
+sleep 2
+echo $green "Done"
+sleep 1
+echo $green "Silahkan Di Pilih"
+sleep 1
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+toilet -f standard "Acep Gans" -F gay
+echo $purple "keluar yah :v"
+sleep 1
+fi
+fi
